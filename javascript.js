479fbdade2782f4aba6c1146f5995b47f347a1e9
ALON PARAG <allon.parag@gmail.com>
2018-06-07 16:08:17 +0300

    updated version

--- javascript.js
+++ javascript.js
@@ -1,53 +1,146 @@
-// === === === === === DEFINING PROPERTIES
-const CLOCK = {};
-Object.defineProperties(CLOCK, {
-	canvas: {
-		value: document.getElementById("timer")
-	}
-});
-Object.defineProperties(CLOCK, {
+const canvas = document.querySelector('#timer-circle');
+const context = canvas.getContext('2d');
+const CLOCK = {
+	timerInit: function() {
+		context.beginPath();
+		context.strokeStyle = "#00A676";
+		context.lineWidth = 2;
+		context.arc(120, 120, 118, 0, 2 * Math.PI);
+		context.stroke();
+	},
+	drawStep: function() {
+		context.beginPath();
+		context.lineWidth = 4;
+		context.fillStyle = CLOCK.whichSession().sessionColor;
+		context.arc(120, 120, 110, Math.PI / 2 - Math.PI * CLOCK.whichSession().timeStep, Math.PI / 2 + Math.PI * CLOCK.whichSession().timeStep);
+		context.stroke();
+		context.fill();
+		document.querySelector('#countdown').innerText = Math.floor(CLOCK.whichSession().timeZero / 60).toString() + ':' + (CLOCK.whichSession().timeZero % 60).toString();
+	},
+	initCount: function(total) {
+		total *= 60
+		if(CLOCK.sessionSwitch) {
+			CLOCK.session.timeZero = total;
+			let localTimeZero = total;
+			CLOCK.session.timeStep = (total - localTimeZero) / total;
+		}
+		else if(!CLOCK.sessionSwitch) {
+			CLOCK.breakProp.timeZero = total;
+			let localTimeZero = total;
+			CLOCK.breakProp.timeStep = (total - localTimeZero) / total;
+		}
+	},
+	clockDisplay: function(total, zero) {
+		document.querySelector('#countdown').innerText = total.toString() + ':00';
+	},
+	timerReset: function() {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		//INITIALIZING FUNCTIONS AND BUTTONS
+	},
+	whichSession: function() {
+		return CLOCK.sessionSwitch ? CLOCK.session : CLOCK.breakProp;
+	},
+	timerCount: function() {
+		if(CLOCK.whichSession().timeStep <= 1) {
+			CLOCK.drawStep();
+			CLOCK.whichSession().timeZero--;
+			CLOCK.whichSession().timeStep = (60 * CLOCK.whichSession().timeTotal - CLOCK.whichSession().timeZero) / (60 * CLOCK.whichSession().timeTotal);
+		}
+		else if(CLOCK.whichSession().timeStep >= 1) {
+			if(CLOCK.sessionSwitch) {
+				// INITIALIZING BREAK COUNT
+				CLOCK.sessionSwitch = false;
+				document.querySelector('#label').innerText = 'Break!';
+				CLOCK.timerReset();
+				CLOCK.timerInit();
+				CLOCK.initCount(CLOCK.breakProp.timeTotal);
+			}
+			else if(!CLOCK.sessionSwitch) {
+				// INITIALIZING SESSION COUNT
+				CLOCK.sessionSwitch = true;
+				document.querySelector('#label').innerText = 'Session';
+				CLOCK.timerReset();
+				CLOCK.timerInit();
+				CLOCK.initCount(CLOCK.session.timeTotal);
+			}
+			//reset the circle
+			//switch the countdown to the correct time
+			CLOCK.drawStep();
+			CLOCK.whichSession().timeZero--;
+			CLOCK.whichSession().timeStep = (60 * CLOCK.whichSession().timeTotal - CLOCK.whichSession().timeZero) / (60 * CLOCK.whichSession().timeTotal);
+		}
+	},
+	timerSwitch: false,
+	sessionSwitch: true,
 	session: {
-		value: {
-			input: document.querySelector('#session-length'),
-			increase: document.querySelector('#session-increase'),
-			decrease: document.querySelector('#session-decrease')
-		},
-		writable: true
+		sessionColor: '#8DCE6A',
+		timeTotal: undefined,
+		timeZero: undefined,
+		timeStep: undefined
 	},
-	break: {
-		value: {
-			input: document.querySelector('#break-length'),
-			increase: document.querySelector('#break-increase'),
-			decrease: document.querySelector('#break-decrease')
-		},
-		writable: true
+	breakProp: {
+		sessionColor: '#A52727',
+		timeTotal: undefined,
+		timeZero: undefined,
+		timeStep: undefined
 	},
-	context: {
-		value: CLOCK.canvas.getContext('2d')
-	}
-});
-//INITIATING INCREASE AND DECREASE FUNCTIONS TO BE CALLED TO EVENTS
-function increase(section) {
-	if(CLOCK[section].input.value > 1 && CLOCK[section].input.value < 99) {
-		CLOCK[section].input.value = (Number(CLOCK[section].input.value) + 1).toString();
-	}
-}
-
-function decrease(section) {
-	if(CLOCK[section].input.value > 1 && CLOCK[section].input.value < 99) {
-		CLOCK[section].input.value = (Number(CLOCK[section].input.value) - 1).toString();
-	}
-}
-// ============== CREATE INITIAL CIRCLE============
+	timerInterval: undefined,
+};
 $(document).ready(function() {
-	CLOCK.context.strokeStyle = "#527A71";
-	CLOCK.context.lineWidth = 5;
-	CLOCK.context.beginPath();
-	CLOCK.context.arc(120, 120, 118, 0, 2 * Math.PI);
-	CLOCK.context.stroke();
-	CLOCK.session.increase.addEventListener('click', function() {
-		if($(this).parent()[0] > 1 && CLOCK['session'].input.value < 99) {
-			CLOCK['session'].input.value = (Number(CLOCK['session'].input.value) - 1).toString();
+	CLOCK.timerInit()
+	CLOCK.clockDisplay(document.querySelector('#session-length input').value);
+	CLOCK.session.timeTotal = Number(document.querySelector('#session-length input').value)
+	CLOCK.breakProp.timeTotal = Number(document.querySelector('#break-length input').value)
+	$('button.increase').click(function() {
+		if($(this)['0'].nextElementSibling.value >= 1 && $(this)['0'].nextElementSibling.value < 99) {
+			if($(this)['0'].parentNode.parentNode.id == 'session-length') {
+				$(this)['0'].nextElementSibling.value++;
+				CLOCK.session.timeTotal++;
+				CLOCK.clockDisplay($(this)[0].nextElementSibling.value);
+			}
+			else if($(this)['0'].parentNode.parentNode.id = 'break-length') {
+				$(this)['0'].nextElementSibling.value++;
+				CLOCK.breakProp.timeTotal++;
+			}
+		}
+	});
+	$('button.decrease').click(function() {
+		if($(this)['0'].previousElementSibling.value > 1 && $(this)['0'].previousElementSibling.value <= 99) {
+			console.log($(this)['0'].parentNode.parentNode.id)
+			$(this)['0'].previousElementSibling.value--;
+			if($(this)['0'].parentNode.parentNode.id == 'session-length') {
+				CLOCK.session.timeTotal--;
+				CLOCK.clockDisplay($(this)[0].previousElementSibling.value);
+			}
+			else if($(this)['0'].parentNode.parentNode.id = 'break-length') {
+				CLOCK.breakProp.timeTotal--;
+			}
+		}
+	});
+	$('input').on('keyup', function() {
+		if(this.parentNode.id = 'session-length') {
+			CLOCK.session.timeTotal = Number(this.value);
+			CLOCK.clockDisplay(this.value);
+		}
+		else if(this.parentNode.id = 'break-length') {
+			CLOCK.breakProp.timeTotal = Number(this.value);
+		}
+	});
+	$('#timer-count').on('click', function() {
+		if(!CLOCK.timerSwitch) {
+			CLOCK.initCount(CLOCK.session.timeTotal);
+			CLOCK.timerInterval = setInterval(CLOCK.timerCount, 1000);
+			CLOCK.timerSwitch = true;
+			$('button, input').prop('disabled', true);
+		}
+		else if(CLOCK.timerSwitch) {
+			clearInterval(CLOCK.timerInterval);
+			CLOCK.timerSwitch = false;
+			CLOCK.sessionSwitch = true;
+			CLOCK.clockDisplay(CLOCK.session.timeTotal, )
+			CLOCK.timerReset();
+			CLOCK.timerInit();
+			$('button, input').prop('disabled', false);
 		}
 	});
 });
